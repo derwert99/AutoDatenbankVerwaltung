1c3ed8e5eaed1eb150eb9ec2db27881cb5411bd5
derwert99 <153212246+derwert99@users.noreply.github.com>
2023-12-14 18:45:35 +0100

    Redudante Klassen entfernt ArrayListen hinzugefügt, Credentials Datenbank erstellt Automodell Datenbank erstellt
    access Klassen erstellt

---
+++ source/src/AccessCredentials.java
@@ -0,0 +1,10 @@
+public class AccessCredentials {
+
+
+
+
+
+    public  void  accessCredentials(){
+        String credentialsPfad = "C:\\Users\\43660\\OneDrive - Fachhochschule Burgenland GmbH\\Semester 3\\Programmiertechniken\\Gruppenarbeit\\Git\\AutoDatenbankVerwaltung\\source\\DataStore\\credentials.db";
+    }
+}
---
+++ source/src/AccessFahrzeuge.java
@@ -0,0 +1,24 @@
+public static class AccessFahrzeuge {
+
+
+
+
+    // must be created yet
+
+
+
+
+
+
+
+    public void accessAutomodelle (){
+        String automodellePfad = "C:\\Users\\43660\\OneDrive - Fachhochschule Burgenland GmbH\\Semester 3\\Programmiertechniken\\Gruppenarbeit\\Git\\AutoDatenbankVerwaltung\\source\\DataStore\\Automodelle.db";
+    }
+
+    public void accessMotorradmodelle (){
+        String motorradmodellePfad = "";
+    }
+
+
+
+}
